7d570b18f75dfe799f1adb2ef20bc0be5de8199f
Mahipal Singh Jhala <mahipalsinghjhala707@gmail.com>
2024-04-14 20:50:03 +0530

    3

---
+++ logic_building_3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n ,flag1=0,flag2=0;
+    cin>>n;
+    for (int i = 0; i < 10; i++)
+    {   if(i%2==0){
+            if((n-1)%3==0){
+                cout<<"Girl won"<<endl;
+            }
+            else if((n-1)%3==0){
+                cout<<"Girl won"<<endl;
+                }
+    }
+    }
+    
+    return 0;
+}
